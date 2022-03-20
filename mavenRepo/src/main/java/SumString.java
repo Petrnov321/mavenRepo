33b12906327c4d21e09a9689d2228747733a0719
Petrnov321 <98708359+Petrnov321@users.noreply.github.com>
2022-03-20 20:06:46 +0100

    první verze

---
+++ mavenRepo/src/main/java/SumString.java
@@ -0,0 +1,10 @@
+public class SumString {
+
+
+
+    public String uniteStrings(String s, String t){
+            String unite = s + t;
+            return unite;
+    }
+
+}
---
+++ mavenRepo/src/test/java/testSumString.java
@@ -0,0 +1,9 @@
+public class testSumString {
+
+    public static void main(String[] args) {
+        SumString unite = new SumString();
+
+        System.out.println(unite.uniteStrings("Nazdar", " světe"));
+    }
+
+}
